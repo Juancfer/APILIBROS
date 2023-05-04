79ccd773314cdec58a4fba4151fa4abaef8634ad
Juan Carlos Fernandez <juanf1986@gmail.com>
2023-05-04 22:07:19 +0200

    Ejericio API Book

---
+++ models/Book.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    pages: {
+      type: Number,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+module.exports = { Book };
